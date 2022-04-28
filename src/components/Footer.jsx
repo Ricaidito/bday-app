94bcd9f49e5346a1287739d94e007221c7815ad8
Carlos Collado <carlose0808@hotmail.com>
2022-04-28 17:49:06 -0400

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 import "../styles/Footer.css";
 
-const Footer = () =>{
-    return(
-        <footer className='footer'>
-            <p>LeafSoft © Todos los derechos reservados | 2022</p>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer
+      id="sticky-footer"
+      className="flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom"
+    >
+      <p className="text-center">
+        LeafSoft © Todos los derechos reservados | 2022
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
